cbf6fd3d7073809b4bcccea49dc4e1ce50754c6b
fourlen <oborin.2013@mail.ru>
2021-08-03 00:16:48 +0500

    fix: rebuilt post request, renamed functions and added requirements

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.utils import redirect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///itmo.db'
@@ -18,68 +17,54 @@ class Values(db.Model):
         return '<Values %r>' % self.id
 
 
-@app.route('/')
-def index():
-    values = Values.query.first()
-    return f"Участников: {values.members}\nПроектов: {values.projects}\nСобытий: {values.events}"
-
 @app.route('/members/get')
-def getmembers():
+def get_members():
     values = Values.query.first()
     return str(values.members)
 
 
-@app.route('/members/post', methods=['POST', 'GET'])
-def postmembers():
+@app.route('/members/set', methods=['POST'])
+def set_members():
     values = Values.query.first()
-    if request.method == 'POST':
-        values.members = request.form['members']
-        try:
-            db.session.commit()
-            return "Изменение прошло успешно"
-        except:
-            return "Ошибка рекдактирования"
-    else:
-        return "Редактирование: members"
+    values.members = request.form['members']
+    try:
+        db.session.commit()
+        return "Изменение прошло успешно"
+    except:
+        return "Ошибка рекдактирования"
 
 @app.route('/projects/get')
-def getprohects():
+def get_projects():
     values = Values.query.first()
     return str(values.projects)
 
 
-@app.route('/projects/post', methods=['POST', 'GET'])
-def postprojects():
+@app.route('/projects/set', methods=['POST'])
+def set_projects():
     values = Values.query.first()
-    if request.method == 'POST':
-        values.projects = request.form['projects']
-        try:
-            db.session.commit()
-            return "Изменение прошло успешно"
-        except:
-            return "Ошибка рекдактирования"
-    else:
-        return "Редактирование: projects"
+    values.projects = request.form['projects']
+    try:
+        db.session.commit()
+        return "Изменение прошло успешно"
+    except:
+        return "Ошибка рекдактирования"
 
 
 @app.route('/events/get')
-def getevents():
+def get_events():
     values = Values.query.first()
     return str(values.events)
 
 
-@app.route('/events/post', methods=['POST', 'GET'])
-def postevents():
+@app.route('/events/set', methods=['POST'])
+def set_events():
     values = Values.query.first()
-    if request.method == 'POST':
-        values.events = request.form['events']
-        try:
-            db.session.commit()
-            return "Изменение прошло успешно"
-        except:
-            return "Ошибка рекдактирования"
-    else:
-        return "Редактирование: events"
+    values.events = request.form['events']
+    try:
+        db.session.commit()
+        return "Изменение прошло успешно"
+    except:
+        return "Ошибка рекдактирования"
 
 
 if __name__ == "__main__":
