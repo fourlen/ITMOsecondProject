42bff56ad8518ec1d34848835a336e799fea7b9b
fourlen <oborin.2013@mail.ru>
2021-08-02 22:53:57 +0500

    fix: added different functions for each field

--- main.py
+++ main.py
@@ -23,20 +23,64 @@ def index():
     values = Values.query.first()
     return f"Участников: {values.members}\nПроектов: {values.projects}\nСобытий: {values.events}"
 
+@app.route('/members/get')
+def getmembers():
+    values = Values.query.first()
+    return str(values.members)
 
-@app.route('/update', methods=['POST', 'GET'])
-def update():
+
+@app.route('/members/post', methods=['POST', 'GET'])
+def postmembers():
     values = Values.query.first()
     if request.method == 'POST':
         values.members = request.form['members']
-        values.projects = request.form['projects']
-        values.events = request.form['events']
         try:
             db.session.commit()
-            return redirect('/')
+            return "Изменение прошло успешно"
         except:
             return "Ошибка рекдактирования"
     else:
-        return "Редактирование"
+        return "Редактирование: members"
+
+@app.route('/projects/get')
+def getprohects():
+    values = Values.query.first()
+    return str(values.projects)
+
+
+@app.route('/projects/post', methods=['POST', 'GET'])
+def postprojects():
+    values = Values.query.first()
+    if request.method == 'POST':
+        values.projects = request.form['projects']
+        try:
+            db.session.commit()
+            return "Изменение прошло успешно"
+        except:
+            return "Ошибка рекдактирования"
+    else:
+        return "Редактирование: projects"
+
+
+@app.route('/events/get')
+def getevents():
+    values = Values.query.first()
+    return str(values.events)
+
+
+@app.route('/events/post', methods=['POST', 'GET'])
+def postevents():
+    values = Values.query.first()
+    if request.method == 'POST':
+        values.events = request.form['events']
+        try:
+            db.session.commit()
+            return "Изменение прошло успешно"
+        except:
+            return "Ошибка рекдактирования"
+    else:
+        return "Редактирование: events"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
